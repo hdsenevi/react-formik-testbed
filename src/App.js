80cbd6ab2434b74e88f814b1c7d12221d90490ca
hdsenevi <shanaka36@gmail.com>
2020-11-12 12:26:01 +1100

    adding initial values

--- src/App.js
+++ src/App.js
@@ -3,12 +3,38 @@ import './App.css';
 import { withFormik } from 'formik';
 import Yup from 'yup';
 
-function App() {
+function App({
+  values,
+  handleChange,
+}) {
   return (
     <div>
-      test text
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={values.email} 
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={values.password} 
+        onChange={handleChange}
+      />
+      
     </div>
   );
 }
 
-export default App;
+const FormikApp = withFormik({
+  mapPropsToValues({email, password}) {
+    return {
+      email: email || 'test text',
+      password: password || "",
+    }
+  }
+})(App)
+
+export default FormikApp;
