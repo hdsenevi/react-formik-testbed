4a9c1f6848c67b9d0c76b32a58ff39c08c954ffa
hdsenevi <shanaka36@gmail.com>
2020-11-13 12:53:15 +1100

    drop down and a checkbox

--- src/App.js
+++ src/App.js
@@ -26,17 +26,27 @@ function App({
         <Field type="checkbox" name="newsletter" checked={values.newsletter} />
         Join our newsletter
       </label>
+      <Field component="select" name="plan">
+        <option value="free">Free</option>
+        <option value="premium">Premium</option>
+      </Field>
       <button>Submit</button>
     </Form>
   );
 }
 
 const FormikApp = withFormik({
-  mapPropsToValues({email, password, newsletter}) {
+  mapPropsToValues({
+    email,
+    password,
+    newsletter,
+    plan,
+  }) {
     return {
       email: email || 'test text',
       password: password || "",
       newsletter: newsletter || false,
+      plan: plan || 'free',
     }
   },
   handleSubmit(values) {
